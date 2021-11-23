cdee4c97c84b2a306133926eed5ba44a807796c2
FatmaGaber-code <94838615+FatmaGaber-code@users.noreply.github.com>
2021-11-23 11:51:56 +0200

    Add files via upload

---
+++ PY0101EN-2-2-Lists answers.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+
+a_list = [1, 'hello', [1, 2, 3] , True]
+a_list
+
+
+# In[3]:
+
+
+a_list[1]
+
+
+# In[4]:
+
+
+A = [1, 'a'] 
+B = [2, 1, 'd']
+A + B
+
+
+# In[ ]:
+
+
+
+
