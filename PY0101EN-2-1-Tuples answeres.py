cb0dba81d1b5ec693b8a3c95b1823df62e8d189d
FatmaGaber-code <94838615+FatmaGaber-code@users.noreply.github.com>
2021-11-23 11:27:07 +0200

    Add files via upload

---
+++ PY0101EN-2-1-Tuples answeres.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Create your first tuple
+tuple1 = ("disco",10,1.2 )
+tuple1
+
+
+# In[2]:
+
+
+# Print the type of the tuple you created
+type(tuple1)
+
+
+# In[3]:
+
+
+# Print the variable on each index
+print(tuple1[0])
+print(tuple1[1])
+print(tuple1[2])
+
+
+# In[4]:
+
+
+# Print the type of value on each index
+print(type(tuple1[0]))
+print(type(tuple1[1]))
+print(type(tuple1[2]))
+
+
+# In[9]:
+
+
+tuple2 = tuple1 + ("hard rock", 10)
+tuple2
+
+
+# In[5]:
+
+
+# sample tuple
+genres_tuple = ("pop", "rock", "soul", "hard rock", "soft rock", "R&B", "progressive rock", "disco")
+genres_tuple
+
