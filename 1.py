680166680d0dba58bccb556cc34f189126a870da
FatmaGaber-code <94838615+FatmaGaber-code@users.noreply.github.com>
2021-11-23 10:52:13 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# print('Hello, Python!') # This line prints a string
+print('Hello, Python!')
+
+
+# In[2]:
+
+
+type(2.14)
+
+
+# In[3]:
+
+
+type(-1)
+
+
+# In[4]:
+
+
+type(float(2))
+
+
+# In[5]:
+
+
+type(True)
+
+
+# In[6]:
+
+
+type (6 / 2)
+
+
+# In[7]:
+
+
+type (6 // 2)
+
+
+# In[8]:
+
+
+# Mathematical expression
+30 + 2 * 60
+
+
+# In[9]:
+
+
+# Mathematical expression
+(30 + 2) * 60
+
+
+# In[13]:
+
+
+x = 3 + 2 * 2
+x
+
+
+# In[14]:
+
+
+y = (3 + 2) * 2
+y
+
+
+# In[15]:
+
+
+z = x + y
+z
+
+
+# In[ ]:
+
+
+
+
